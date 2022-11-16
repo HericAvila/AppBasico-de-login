deb32fd4af1873ef42cfda346cf3ad7befa5a20e
Heric Avila <avilaheric@ucl.br>
2022-11-16 12:56:37 -0300

    Tela com drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Fala corno'),
     );
   }
 }
@@ -40,50 +29,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          children: [
-            Icon(Icons.abc),
-            Icon(Icons.confirmation_num_sharp),
-            //Image.asset('lib/image/gdl_logo.jpg')
-            Image.asset('assets/images/gdl_logo.jpg')
-          ],
-        ),
+      drawer: Drawer(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Gaveta de Pastas'),
+            ),
+            body: Container(
+              child: ListView(
+                children: [
+                  ListBody(
+                    children: [
+                      UserAccountsDrawerHeader(
+                        accountName: Text('Heric Avila'),
+                        accountEmail:
+                            Text('heric.emerenciano@gdllogistica.com.br'),
+                        currentAccountPicture: Icon(
+                          Icons.supervised_user_circle_outlined,
+                          size: 70,
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('Nova linha'),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
